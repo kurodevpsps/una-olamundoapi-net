9e9cc52c1029732ec026c219c698a2a82620b770
kurodevpsps <abraaogalmeida@gmail.com>
2026-03-22 23:07:23 -0300

    Add files via upload

---
+++ OlaMundoController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OlaMundoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OlaMundoController : ControllerBase
+    {
+          [HttpGet]
+          public string Get()
+        {
+            return "Olá, Mundo!";
+        }
+    }
+}
